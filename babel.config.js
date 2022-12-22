68ed769f6d432d69e1a8adde71227836cf48b4b8
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-22 20:56:22 -0300

    chore(config): setup initial configuration
    add conventional commit, path mapping, node version, editor formatter

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@config': './src/config',
+            '@constants': './src/constants',
+            '@context': './src/context',
+            '@dtos': './src/dtos',
+            '@hooks': './src/hooks',
+            '@models': './src/models',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@storage': './src/storage',
+            '@styles': './src/styles',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
