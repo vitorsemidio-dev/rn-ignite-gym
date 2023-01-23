cfebba4de77197ac9d8dccb3835eb29c2a0604d9
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-01-23 01:50:41 -0300

    feat(screen): finish layout signin screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,42 +2,53 @@ import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
-import { Center, Heading, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, Text, VStack, ScrollView } from "native-base";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700" px={10}>
-      <Image
-        source={BackgroundImg}
-        alt="Pessoas treinando"
-        resizeMode="contain"
-        position="absolute"
-      />
-
-      <Center my={24}>
-        <LogoSvg />
-
-        <Text color="gray.100" fontSize="sm">
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
-
-      <Center>
-        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-          Crie sua conta
-        </Heading>
-
-        <Input
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCapitalize="none"
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="gray.700" px={10} pb={10}>
+        <Image
+          source={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
         />
-        <Input placeholder="Senha" secureTextEntry />
 
-        <Button title="Acessar" />
-      </Center>
+        <Center my={24}>
+          <LogoSvg />
 
-      <Button title="Criar conta" variant="outline" />
-    </VStack>
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Crie sua conta
+          </Heading>
+
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry />
+
+          <Button title="Acessar" />
+        </Center>
+
+        <Center mt="24">
+          <Text color="white" fontSize="sm" fontFamily="body" mb={3}>
+            Ainda não tem acesso?
+          </Text>
+
+          <Button title="Criar conta" variant="outline" />
+        </Center>
+      </VStack>
+    </ScrollView>
   );
 }
