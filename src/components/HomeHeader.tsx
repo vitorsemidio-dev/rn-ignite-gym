d0b9f76a4a9d55cf1fa7ccb5732e06ae39343c94
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-01-26 03:26:55 -0300

    feat(component): create HomeHeader component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,28 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { Heading, HStack, Icon, Text, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { UserPhoto } from "./UserPhoto";
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: "https://github.com/vitorsemidio-dev.png" }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
+      <VStack flex={1} mr={4}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Emídio
+        </Heading>
+      </VStack>
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
