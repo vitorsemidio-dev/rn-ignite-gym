fbf73ca2ec7fc6efe4224acb03c4cc9630aa1776
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-01-26 05:07:57 -0300

    feat(component): create ScreenHeader

---
+++ src/components/ScreenHeader.tsx
@@ -0,0 +1,15 @@
+import { Center, Heading } from "native-base";
+
+type Props = {
+  title: string;
+};
+
+export function ScreenHeader({ title }: Props) {
+  return (
+    <Center bg="gray.600" pt={16} pb={6}>
+      <Heading fontSize="xl" color="gray.100">
+        {title}
+      </Heading>
+    </Center>
+  );
+}
