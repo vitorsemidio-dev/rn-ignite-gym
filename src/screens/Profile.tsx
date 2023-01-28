d7d9bcfcc225ef1ddcb6c77333adc6f8a92a2a9b
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-01-28 15:49:36 -0300

    feat(profile): add select image to update user photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -3,7 +3,16 @@ import { Heading } from "@components/Heading";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, ScrollView, Skeleton, Text, VStack } from "native-base";
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
+import {
+  Center,
+  ScrollView,
+  Skeleton,
+  Text,
+  useToast,
+  VStack,
+} from "native-base";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
 
@@ -11,6 +20,52 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/vitorsemidio-dev.png"
+  );
+  const toast = useToast();
+
+  async function handleUserPhotoSelected() {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets.length === 0) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+
+        const MAX_SIZE_MB = 5;
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > MAX_SIZE_MB) {
+          return toast.show({
+            title: `Essa imagem é muito grande. Escolha uma de até ${MAX_SIZE_MB}MB.`,
+            placement: "top",
+            bgColor: "red.500",
+          });
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -32,12 +87,12 @@ export function Profile() {
             ></Skeleton>
           ) : (
             <UserPhoto
-              source={{ uri: "https://github.com/vitorsemidio-dev.png" }}
+              source={{ uri: userPhoto }}
               alt="Avatar usuário logado"
               size={PHOTO_SIZE}
             />
           )}
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelected}>
             <Text
               color="green.500"
               fontWeight="bold"
