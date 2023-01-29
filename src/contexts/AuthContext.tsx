1ceb62b22b9de02b93d67f100db20e3eb03bfbba
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-01-28 22:23:59 -0300

    feat(storage): save user logged in device storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,12 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
-import { createContext, ReactNode, useState } from "react";
+import { storageUserGet, storageUserSave } from "@storage/storageUser";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 export type AuthContextDataProps = {
   user: UserDTO;
   singIn: (email: string, password: string) => Promise<void>;
+  isLoadingUserStorageData: boolean;
 };
 
 type AuthContextProviderProps = {
@@ -21,7 +23,23 @@ export const AuthContext = createContext<AuthContextDataProps>(
 );
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
+    useState(true);
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
+
+  async function loadUserData() {
+    try {
+      const userLogged = await storageUserGet();
+
+      if (userLogged) {
+        setUser(userLogged);
+      }
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
 
   async function singIn(email: string, password: string): Promise<void> {
     try {
@@ -32,17 +50,23 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       if (data.user) {
         setUser(data.user);
+        await storageUserSave(data.user);
       }
     } catch (error) {
       throw error;
     }
   }
 
+  useEffect(() => {
+    loadUserData();
+  }, []);
+
   return (
     <AuthContext.Provider
       value={{
         user,
         singIn,
+        isLoadingUserStorageData,
       }}
     >
       {children}
